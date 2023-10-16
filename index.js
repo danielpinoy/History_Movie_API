39f9a1c564bbc8909c55bebeb1d4161042f51de1
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2023-10-16 16:26:17 +0800

    added api

--- index.js
+++ index.js
@@ -3,133 +3,43 @@ const express = require("express"),
     bodyParser = require("body-parser"),
     { v4: uuidv4 } = require("uuid");
 
+const mongoose = require("mongoose");
+Models = require("./model.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Director = Models.Director;
+mongoose
+    .connect("mongodb://127.0.0.1:27017/history_movies", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to the database.");
+    })
+    .catch((err) => {
+        console.error("Database connection error:", err);
+    });
+
 const app = express();
 
 app.use(bodyParser.json());
 
-historicBooks = [
-    {
-        id: 1,
-        title: "Babylon",
-        director: {
-            name: "Damien Chazelle",
-            bio: "Damien Chazelle is an American director known for his exceptional work in drama and musical genres. Born in 1985, he has received critical acclaim for his contributions to the film industry.",
-            birthYear: 1985,
-        },
-        criticRating: 9.0,
-        userRating: 8.5,
-        genres: ["Drama", "Musical"],
-    },
-    {
-        id: 2,
-        title: "Dunkirk",
-        director: {
-            name: "Christopher Nolan",
-            bio: "Christopher Nolan is a British-American filmmaker known for his remarkable achievements in war and action genres. Born in 1970, he has left a significant impact on cinema.",
-            birthYear: 1970,
-        },
-        criticRating: 8.5,
-        userRating: 8.0,
-        genres: ["War", "Action"],
-    },
-    {
-        id: 3,
-        title: "The Death of Stalin",
-        director: {
-            name: "Armando Iannucci",
-            bio: "Armando Iannucci is a Scottish satirist and director who has made notable contributions to the world of comedy and drama. Born in 1963, his work is celebrated for its unique wit and humor.",
-            birthYear: 1963,
-        },
-        criticRating: 7.8,
-        userRating: 7.2,
-        genres: ["Comedy", "Drama"],
-    },
-    {
-        id: 4,
-        title: "Waterloo",
-        director: {
-            name: "Sergey Bondarchuk",
-            bio: "Sergey Bondarchuk was a Soviet actor and filmmaker who made significant contributions to war and drama genres. Born in 1920, he is remembered for his impactful work in cinema.",
-            birthYear: 1920,
-        },
-        criticRating: 8.2,
-        userRating: 8.1,
-        genres: ["War", "Drama"],
-    },
-    {
-        id: 5,
-        title: "Citizen Kane",
-        director: {
-            name: "Orson Welles",
-            bio: "Orson Welles was an American actor and director renowned for his work in drama and mystery genres. Born in 1915, he left a lasting legacy in the world of cinema.",
-            birthYear: 1915,
-        },
-        criticRating: 9.5,
-        userRating: 9.0,
-        genres: ["Drama", "Mystery"],
-    },
-    {
-        id: 6,
-        title: "All Quiet on the Western Front",
-        director: {
-            name: "Edward Berger",
-            bio: "Edward Berger is a German director known for his contributions to war and drama genres. Born in 1970, his work has been critically acclaimed.",
-            birthYear: 1970,
-        },
-        criticRating: 8.7,
-        userRating: 8.3,
-        genres: ["War", "Drama"],
-    },
-    {
-        id: 7,
-        title: "Saving Private Ryan",
-        director: {
-            name: "Steven Spielberg",
-            bio: "Steven Spielberg is an American filmmaker known for his exceptional work in war and action genres. Born in 1946, he has had a profound impact on the film industry.",
-            birthYear: 1946,
-        },
-        criticRating: 8.9,
-        userRating: 8.7,
-        genres: ["War", "Action"],
-    },
-    {
-        id: 8,
-        title: "Zulu",
-        director: {
-            name: "Cy Endfield",
-            bio: "Cy Endfield was an American filmmaker known for his contributions to war and action genres. Born in 1914, his work has been celebrated for its cinematic excellence.",
-            birthYear: 1914,
-        },
-        criticRating: 8.0,
-        userRating: 7.5,
-        genres: ["War", "Action"],
-    },
-    {
-        id: 9,
-        title: "Gladiator",
-        director: {
-            name: "Ridley Scott",
-            bio: "Ridley Scott is an English filmmaker recognized for his work in action and drama genres. Born in 1937, he has created some of cinema's iconic works.",
-            birthYear: 1937,
-        },
-        criticRating: 8.6,
-        userRating: 8.2,
-        genres: ["Action", "Drama"],
-    },
-    {
-        id: 10,
-        title: "The King's Speech",
-        director: {
-            name: "Tom Hooper",
-            bio: "Tom Hooper is an English film and television director known for his work in drama and biography films. Born in 1972, he has made significant contributions to storytelling in cinema.",
-            birthYear: 1972,
-        },
-        criticRating: 8.4,
-        userRating: 8.0,
-        genres: ["Drama", "Biography"],
-    },
-];
-
+// const historicBooks = {
+//     Title: "Saving Private Ryan",
+//     Description: "Saving Private Ryan is a 1998 American war film directed by Steven Spielberg...",
+//     Genre: {
+//         Name: "War",
+//         Description: "War films depict the experiences of war, including its effects on society...",
+//     },
+//     Director: {
+//         Name: "Steven Spielberg",
+//         Bio: "Steven Spielberg is an American filmmaker known for his exceptional work in war and action genres...",
+//     },
+//     Actors: ["Tom Hanks", "Matt Damon", "Tom Sizemore", "Edward Burns", "Barry Pepper"],
+//     Featured: true,
+// };
 users = [{ id: 1, username: "Daniel", password: "123", favoriteMovies: [] }];
 
 // Everything in the public folder will be served
@@ -147,129 +57,190 @@ app.get("/documentation", (req, res) => {
     const filePath = "public/documentation.html";
     res.sendFile(filePath, { root: __dirname });
 });
-// Add new user
-app.post("/register", (req, res) => {
-    const { username, password } = req.body;
-
-    // If username already taken
-    if (users.find((user) => user.username === username)) {
-        return res.status(400).json({ error: "Username already exists" });
-    }
-    const userId = uuidv4();
-
-    const newUser = { id: userId, username, password };
-
-    users.push(newUser);
-
-    res.status(201).json({ message: "User registered successfully" });
-});
 
 // get
 app.get("/users", (req, res) => {
-    res.json(users);
+    Users.find()
+        .then((Users) => {
+            res.status(201).json(Users);
+        })
+        .catch((error) => {
+            console.error("Mongoose query error:", error);
+            res.status(500).send("Error: " + error);
+        });
+});
+
+// Add new user
+app.post("/register", (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + "Already user inside Database");
+            } else {
+                Users.create({
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday,
+                    FavoriteMovies: req.body.FavoriteMovies,
+                })
+                    .then((user) => {
+                        res.status(201).json(user);
+                    })
+                    .catch((error) => {
+                        console.error("Mongoose query error:", error);
+                        res.status(500).send("Error: " + error);
+                    });
+            }
+        })
+        .catch((error) => {
+            console.error("Error inserting user:", error);
+            res.status(500).json({ error: "Internal server error" });
+        });
 });
 
 // Update User
-app.put("/users/:id", (req, res) => {
-    const { id } = req.params;
-    const { newUsername } = req.body;
+app.put("/users/:Username", async (req, res) => {
+    try {
+        const updatedUser = await Users.findOneAndUpdate(
+            { Username: req.params.Username },
+            {
+                $set: {
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday,
+                },
+            },
+            { new: true }
+        );
 
-    const userToUpdate = users.find((user) => user.id == id);
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
 
-    if (userToUpdate) {
-        userToUpdate.username = newUsername;
-        res.status(200).send("Username updated successfully");
-    } else {
-        return res.status(404).send("User not found");
+        res.json(updatedUser);
+    } catch (error) {
+        console.error("Error updating user:", error);
+        res.status(500).send("Error: " + error.message);
     }
 });
 //
 
 // add favorite movie to user
-app.post("/users/:id/:movieTitle", (req, res) => {
-    const { id, movieTitle } = req.params;
+app.post("/users/addfavorite", (req, res) => {
+    const { userId, movieId } = req.body;
 
-    const user = users.find((user) => user.id == id);
-
-    if (user) {
-        user.favoriteMovies.push(movieTitle);
-        res.status(201).send(`${movieTitle} have been added to user ${id}`);
-    } else {
-        res.status(400).send("User not found");
-    }
+    Users.findByIdAndUpdate(
+        userId,
+        { $addToSet: { FavoriteMovies: movieId } }, // $addToSet ensures no duplicates
+        { new: true }
+    )
+        .then((updatedUser) => {
+            if (!updatedUser) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            res.status(200).json(updatedUser);
+        })
+        .catch((error) => {
+            console.error("Error adding favorite movie:", error);
+            res.status(500).json({ error: "Internal server error" });
+        });
 });
 
 // Delete
 app.delete("/users/:id/:movieTitle", (req, res) => {
     const { id, movieTitle } = req.params;
 
-    let user = users.find((user) => user.id !== id);
-
-    if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter((title) => title !== movieTitle);
-        res.status(201).send(`${movieTitle} have been removed to user ${id}`);
-    } else {
-        res.status(400).send("User not found");
-    }
+    Users.updateOne({ _id: id }, { $pull: { FavoriteMovies: movieTitle } })
+        .then((updateResult) => {
+            if (updateResult.nModified === 0) {
+                return res
+                    .status(404)
+                    .json({ message: "User not found or movie not in favorites." });
+            }
+            res.status(200).json({ message: "Movie removed from favorites." });
+        })
+        .catch((error) => {
+            console.error("Error removing movie from favorites:", error);
+            res.status(500).json({ error: "Internal server error" });
+        });
 });
 
 // Delete
-
-app.delete("/users/:id/", (req, res) => {
+app.delete("/users/:id", (req, res) => {
     const { id } = req.params;
-
-    let user = users.find((user) => user.id == id);
-
-    if (user) {
-        users = users.filter((user) => user.id != id);
-        res.status(201).send(`user ${id} have been removed`);
-        // res.json(users);
-    } else {
-        res.status(400).send("User not found");
-    }
+    Users.findOneAndRemove({ _id: id })
+        .then((deletedUser) => {
+            // Use the correct variable name here
+            if (!deletedUser) {
+                res.status(404).json({ error: "User Not Found" }); // Adjust the status code to 404 for "Not Found"
+            } else {
+                console.log("User deleted:", deletedUser);
+                res.status(200).json({ message: "User deleted successfully" });
+            }
+        })
+        .catch((error) => {
+            console.error("Error deleting user:", error);
+            res.status(500).json({ error: "Internal server error" });
+        });
 });
 
+// ////////////////////////////////////////////////////////////
+//                            MOVIES
+// //////////////////////////////////////////////////////
 // get all movies
 app.get("/historic-movies", (req, res, next) => {
-    res.json(historicBooks);
+    Movies.find()
+        .then((movies) => {
+            // Process the result
+            res.status(201).json(movies);
+        })
+        .catch((error) => {
+            console.error("Mongoose query error:", error);
+            res.status(500).send("Error: " + error);
+        });
 });
 
 // return movie by title
-app.get("/historic-movies/:title", (req, res) => {
-    const { title } = req.params;
-
-    const movies = historicBooks.find((movie) => movie.title === title);
-    console.log(movies);
-    if (!movies) {
-        res.status(404).send("Movie not found ");
-    }
-
-    res.json(movies);
+app.get("/historic-movies/:Title", (req, res) => {
+    Movies.findOne({ Title: req.params.Title })
+        .then((movie) => {
+            if (movie) {
+                res.status(201).json(movie);
+            } else {
+                res.status(500).send("Movie not found");
+            }
+        })
+        .catch((error) => {
+            console.error("Mongoose query error:", error);
+            res.status(500).send("Error: " + error);
+        });
 });
 
 // return movie by genre
-app.get("/historic-movies/genres/:genre", (req, res) => {
-    const { genre } = req.params;
-
-    const movies = historicBooks.filter((movie) => movie.genres.includes(genre));
-
-    if (movies) {
-        res.json(movies);
-    } else {
-        res.status(404).send("No movies with this genre");
-    }
+app.get("/historic-movies/genres/:Genre", (req, res) => {
+    Movies.find({ "Genre.Name": req.params.Genre })
+        .then((movie) => {
+            res.status(201).json(movie);
+        })
+        .catch((error) => {
+            console.error("Mongoose query error:", error);
+            res.status(500).send("Error: " + error);
+        });
 });
 
 // return director by name
-app.get("/historic-movies/director/:name", (req, res) => {
-    const { name } = req.params;
-
-    const movies = historicBooks.find((movie) => movie.director.name === name);
-    if (movies) {
-        res.json(movies.director);
-    } else {
-        res.status(404).send("Director not found");
-    }
+app.get("/historic-movies/Director/:Name", (req, res) => {
+    Movies.find({ "Director.Name": req.params.Name })
+        .then((movie) => {
+            const Director = movie[0].Director;
+            res.status(201).json(Director);
+        })
+        .catch((error) => {
+            console.error("Mongoose query error:", error);
+            res.status(500).send("Error: " + error);
+        });
 });
 // error function
 app.use((err, req, res, next) => {
