d2a04584b1a17fd08770d055f471731fa8973225
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2024-02-01 14:34:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ let allowedOrigins = [
     "http://testsite.com",
     "http://localhost:1234",
     "https://kaleidoscopic-tiramisu-9e52da.netlify.app",
-    "http://localhost:4200/",
+    "http://localhost:4200",
 ];
 
 app.use(
